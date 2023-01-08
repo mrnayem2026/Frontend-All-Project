ab6b92b52b6a6461f0e9520d830833eb8ce60685
Mostafizur Rahman Nayem <hpbd17062022@gmail.com>
2023-01-08 12:51:17 +0600

    Navbar Added

--- Figma to HTML/Lawyer Landing page/main.js
+++ Figma to HTML/Lawyer Landing page/main.js
@@ -1,5 +1,11 @@
 //? <<<<>>> Base Sass Folder link is below <<<<>>>
-
 import './sass/base/reset.scss';
+
+//? <<<<>>> Layout Sass Folder link is below <<<<>>>
+import './sass/layout/header.scss';
+
+//? <<<<>>> Variable Sass Folder link is below <<<<>>>
+import './sass/utils/variables.scss';
+
 
 
