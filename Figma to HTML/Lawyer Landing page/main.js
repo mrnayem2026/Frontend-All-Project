769dbecbd96ab2bebef85c94ea2351f01e3e6673
Mostafizur Rahman Nayem <hpbd17062022@gmail.com>
2023-01-08 16:36:43 +0600

    Banner section added one

--- Figma to HTML/Lawyer Landing page/main.js
+++ Figma to HTML/Lawyer Landing page/main.js
@@ -3,7 +3,7 @@ import './sass/base/reset.scss';
 
 //? <<<<>>> Layout Sass Folder link is below <<<<>>>
 import './sass/layout/header.scss';
-
+import './sass/layout/banner.scss';
 //? <<<<>>> Variable Sass Folder link is below <<<<>>>
 import './sass/utils/variables.scss';
 
